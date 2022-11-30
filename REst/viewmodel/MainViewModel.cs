841d7216e3baadd10abd28d13d96b8fe069a6c40
dima-demon <maloy@diceteam.games>
2022-11-30 20:10:22 +0200

    food

---
+++ REst/viewmodel/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace REst.viewmodel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MainViewModel()
+        {
+            Picks = GetPicks();
+        }
+        public List<Pick> Picks { get; set; }
+
+        public ICommand OrderCommand => new Command(() => Application.Current.MainPage.Navigation.PushAsync(new OrderPage()));
+        private List<Pick> GetPicks()
+        {
+            return new List<Pick>
+            {
+                new Pick { Title = "Breakfast", Image = "IMG01.png", Description = "Замовте наший чудовий, корисний, гарячий сніданок " },
+                new Pick { Title = "Lunch", Image = "IMG03.png", Description = "Замовте наший смачний обід, щоб ваш обід був продуктивним та солодким " }
+            };
+        }
+
+    }
+
+    
+    public class Pick
+    {
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+    }
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+    }
+}
---
+++ REst/viewmodel/OrderViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace REst.viewmodel
+{
+    public class OrderViewModel : BaseViewModel
+    {
+        public OrderViewModel()
+        {
+            MenuList = GetMenu();
+        }
+        public List<Pick> MenuList { get; set; }
+
+        public ICommand BackCommand => new Command(() => Application.Current.MainPage.Navigation.PopAsync());
+
+        private List<Pick> GetMenu()
+        {
+            return new List<Pick>
+            {
+                new Pick { Title = "The Ultimate Pack", Image = "IMG05.png", Description = "Гамбургер зі свининою,сиром з перцем та хрустким беконом", Price = "$23.99" },
+                new Pick { Title = "Lamb Bundle", Image = "IMG04.png", Description = "Бараняча ніжка по тайськи з каррі,з молодою кукуродзою,квасолею та перцем чилі", Price = "$19.99" },
+                new Pick { Title = "Juicy Mushroom", Image = "IMG01.png", Description = "Лісові гриби та горох в різотто,з хрусткою смаженою куркою", Price = "$25.25" }
+            };
+        }
+    }
+}
